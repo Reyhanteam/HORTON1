7906071fccfb61d152dff33c12e2194bec323262
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-12 18:36:53 +0330

    feat(models): add role model

---
+++ app/Models/Role.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory; use Illuminate\Database\Eloquent\Model; use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+class Role extends Model { use HasFactory; protected $guarded=[]; public function admins():BelongsToMany{return $this->belongsToMany(AdminUser::class,'role_user');} public function permissions():BelongsToMany{return $this->belongsToMany(Permission::class,'permission_role');} }
