8faefebc89d38428384fa32ce75419f53b64de0e
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-12 18:34:58 +0330

    feat(models): add user profile model

---
+++ app/Models/UserProfile.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+class UserProfile extends Model { use HasFactory; protected $guarded=[]; protected function casts(): array { return ['metadata'=>'array']; } public function user(): BelongsTo { return $this->belongsTo(User::class); } }
