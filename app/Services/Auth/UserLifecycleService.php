d532157f640cf5cd3d0bf2aa6c7fca0dcedbf86e
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-12 19:17:41 +0330

    fix(auth): initialize registered users as pending

--- app/Services/Auth/UserLifecycleService.php
+++ app/Services/Auth/UserLifecycleService.php
@@ -2,16 +2,14 @@
 
 namespace App\Services\Auth;
 
+use App\Enums\UserStatus;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 final class UserLifecycleService extends AbstractUserLifecycle
 {
     protected function createUser(array $attributes): User
     {
-        if (isset($attributes['password'])) {
-            $attributes['password'] = Hash::make($attributes['password']);
-        }
+        $attributes['status'] = UserStatus::Pending;
 
         return User::query()->create($attributes);
     }
