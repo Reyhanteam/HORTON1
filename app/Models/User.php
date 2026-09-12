1a991d9692b539497c0815dc19131e36aa72f891
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-12 18:47:20 +0330

    refactor(models): align user mass assignment with registration fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,16 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([
+    'name',
+    'username',
+    'email',
+    'phone',
+    'password',
+    'status',
+    'locale',
+    'timezone',
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,25 +37,93 @@ class User extends Authenticatable
         });
     }
 
-    public function telegramAccount(): HasOne { return $this->hasOne(TelegramAccount::class); }
-    public function profile(): HasOne { return $this->hasOne(UserProfile::class); }
-    public function orders(): HasMany { return $this->hasMany(Order::class); }
-    public function payments(): HasMany { return $this->hasMany(Payment::class); }
-    public function wallets(): HasMany { return $this->hasMany(Wallet::class); }
-    public function walletTransactions(): HasMany { return $this->hasMany(WalletTransaction::class); }
-    public function services(): HasMany { return $this->hasMany(Service::class); }
-    public function discountUsages(): HasMany { return $this->hasMany(DiscountUsage::class); }
-    public function giftCodeRedemptions(): HasMany { return $this->hasMany(GiftCodeRedemption::class); }
-    public function referralAccount(): HasOne { return $this->hasOne(ReferralAccount::class); }
-    public function referrals(): HasMany { return $this->hasMany(Referral::class, 'referrer_user_id'); }
-    public function referral(): HasOne { return $this->hasOne(Referral::class, 'referred_user_id'); }
-    public function cashbackAccounts(): HasMany { return $this->hasMany(CashbackAccount::class); }
-    public function cashbackTransactions(): HasMany { return $this->hasMany(CashbackTransaction::class); }
-    public function supportTickets(): HasMany { return $this->hasMany(SupportTicket::class); }
-    public function notifications(): HasMany { return $this->hasMany(Notification::class); }
+    public function telegramAccount(): HasOne
+    {
+        return $this->hasOne(TelegramAccount::class);
+    }
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function wallets(): HasMany
+    {
+        return $this->hasMany(Wallet::class);
+    }
+
+    public function walletTransactions(): HasMany
+    {
+        return $this->hasMany(WalletTransaction::class);
+    }
+
+    public function services(): HasMany
+    {
+        return $this->hasMany(Service::class);
+    }
+
+    public function discountUsages(): HasMany
+    {
+        return $this->hasMany(DiscountUsage::class);
+    }
+
+    public function giftCodeRedemptions(): HasMany
+    {
+        return $this->hasMany(GiftCodeRedemption::class);
+    }
+
+    public function referralAccount(): HasOne
+    {
+        return $this->hasOne(ReferralAccount::class);
+    }
+
+    public function referrals(): HasMany
+    {
+        return $this->hasMany(Referral::class, 'referrer_user_id');
+    }
+
+    public function referral(): HasOne
+    {
+        return $this->hasOne(Referral::class, 'referred_user_id');
+    }
+
+    public function cashbackAccounts(): HasMany
+    {
+        return $this->hasMany(CashbackAccount::class);
+    }
+
+    public function cashbackTransactions(): HasMany
+    {
+        return $this->hasMany(CashbackTransaction::class);
+    }
+
+    public function supportTickets(): HasMany
+    {
+        return $this->hasMany(SupportTicket::class);
+    }
+
+    public function notifications(): HasMany
+    {
+        return $this->hasMany(Notification::class);
+    }
 
     protected function casts(): array
     {
-        return ['email_verified_at' => 'datetime', 'phone_verified_at' => 'datetime', 'last_seen_at' => 'datetime', 'password' => 'hashed'];
+        return [
+            'email_verified_at' => 'datetime',
+            'phone_verified_at' => 'datetime',
+            'last_seen_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
