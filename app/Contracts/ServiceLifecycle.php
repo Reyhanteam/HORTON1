0c7056e3724a18e9c8f02a09f8c9ea44077eea45
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-12 20:51:27 +0330

    feat(service): expand service lifecycle contract

--- app/Contracts/ServiceLifecycle.php
+++ app/Contracts/ServiceLifecycle.php
@@ -7,8 +7,13 @@ use App\Models\User;
 
 interface ServiceLifecycle
 {
-    public function create(User $user, int $planId, int $providerId, array $attributes = []): Service;
+    public function create(User $user, int $planId, ?int $providerId = null, array $attributes = []): Service;
+    public function provision(Service $service): Service;
+    public function get(Service $service): Service;
+    public function status(Service $service): Service;
     public function renew(Service $service, int $durationValue, string $durationUnit = 'day'): Service;
+    public function extend(Service $service, int $durationValue, string $durationUnit = 'day'): Service;
     public function extendCapacity(Service $service, int $amount): Service;
     public function disable(Service $service): Service;
+    public function delete(Service $service): Service;
 }
