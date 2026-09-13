378ad971b3747fd862245c5adaf84c36b02f6010
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-13 18:39:05 +0330

    fix(admin): support Jetstream users in Horton RBAC

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,4 +1,31 @@
 <?php
+
+declare(strict_types=1);
+
 namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory; use Illuminate\Database\Eloquent\Model; use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-class Role extends Model { use HasFactory; protected $guarded=[]; public function admins():BelongsToMany{return $this->belongsToMany(AdminUser::class,'role_user');} public function permissions():BelongsToMany{return $this->belongsToMany(Permission::class,'permission_role');} }
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function admins(): BelongsToMany
+    {
+        return $this->belongsToMany(AdminUser::class, 'role_user');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_role');
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'permission_role');
+    }
+}
