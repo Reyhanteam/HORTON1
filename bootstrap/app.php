ea856ec2caef5be58d38fceb6d7f4c7ae116ec4a
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-13 14:56:26 +0330

    feat(notifications): schedule expiry and broadcast dispatch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Console\Commands\DispatchNotificationTasks;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+    })
+    ->withSchedule(function (Schedule $schedule): void {
+        $schedule->command(DispatchNotificationTasks::class)
+            ->everyFiveMinutes()
+            ->withoutOverlapping();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
